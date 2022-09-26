f8bf07b635b7922c330058758a23e1603c30ea9c
Sk0rk1S <91293863+Sk0rk1S@users.noreply.github.com>
2022-09-26 09:09:28 -0700

    Update Structs.c

--- Practice/Structs.c
+++ Practice/Structs.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 
-struct house{
+struct adress{
     char = country;
     char = province;
     char = city;
     int = housenum[192];
 };
 
+void printadress(struct address ad);
+
+int main() {
+
+struct adress ad1;
+struct adress ad2;
+
+
+strxpy(ad1.street,"east x avenue");
+}
