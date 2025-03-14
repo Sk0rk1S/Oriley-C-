c5dfa8d9f6468e84e9a521241698afd7abfc3848
Sk0rk1S <91293863+Sk0rk1S@users.noreply.github.com>
2025-03-13 23:17:15 -0700

    Create GPA_Calc.c

---
+++ Practice/GPA_Calc.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+//First thing Ive ever coded that kinda works March 12 2025!!!
+int main() {
+  int grade;
+  int classes;
+  int sum = 0;
+  float divideBy = 0;
+
+  printf("enter classes\n");
+  scanf("%d", & classes);
+  for (int i = 0; i < classes; i++) {
+    printf("enter grade\n");
+    scanf("%d", & grade);
+    if (grade > 100 || grade < 0) {
+      printf("invalid grade enter again");
+      scanf("%d", & grade);
+    }
+    else if(grade < 50){
+      grade = 0;
+    }
+    sum += grade;
+    divideBy += 23.3;
+  }
+  float gpa = sum / divideBy;
+  printf("Your average GPA is %f\n", gpa);
+}
