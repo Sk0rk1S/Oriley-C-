f6494cffb82af1506c839ea5545edebadc5b5570
Sk0rk1S <91293863+Sk0rk1S@users.noreply.github.com>
2025-03-18 09:33:26 -0700

    Update TriangleMaker.c
    HOLYSHITITWORKS OMGGG IM SO HAPPY

--- Practice/TriangleMaker.c
+++ Practice/TriangleMaker.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+void printStar(int stars){      ///a and b variables were there to help me trace the code easier
+    int sum;                    //To be honest this code was a guess and check and I got lucky
+    char star = 'a';            //Finally...HOLY FUCKIN SHIT IT WORKS!!!!!!!!!
+    char test = 'b';            ///THIS WAS ALL ME NO CHAT GPT STACK OVERFLOW NOTHING HELPED ME
+    for(int i = 0; i < stars; i++){     ///WHICH IS WHY IM CELEBRATING THIS SO MUCH
+        for(int j = 0; j < i; j++){     ///MARCH 18 2025 AT 9:32AM PST I COMPLETED THIS HOLY SHIIITTT
+            printf("%c", star);         ///NO INSTRUCTION MANUAL EITHER LIKE I DID IT OMMMGGG
+        }
+        printf("%c\n", test);
+    }
+}
 
+int main(){
+    int stars;
+    printf("Enter how many stars you'd like");
+    scanf("%d", &stars);
+    printStar(stars);
+}
